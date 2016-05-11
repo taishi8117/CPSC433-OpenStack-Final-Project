fa5c1dee8588b569f2bbaa2d562d68e688ccca96
Alan Liu <alanl.liu@yahoo.com>
2016-05-10 23:42:18 -0400

    added script to run program

---
+++ OpenStack/run_Openstack.sh
@@ -0,0 +1,3 @@
+cd src
+sudo java -cp "project:object:lib:.:lib/json-simple-1.1.1.jar:lib/http-20070405.jar:lib/commons-io-2.5/commons-io-2.5-javadoc.jar:lib/commons-io-2.5/commons-io-2.5.jar" project.OpenStack -config ../openstack.config
+cd ../
